20ee0ce7d7feeeb333a107e9c837c48b66fb4bc3
lukewalbeck <lukewalbeck@gmail.com>
2019-08-08 15:40:40 +0000

    enabled use for axios using cors

--- api/settings.py
+++ api/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['lwalbeck.pythonanywhere.com']
 
 
 # Application definition
+CORS_ORIGIN_ALLOW_ALL=True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'recipes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
