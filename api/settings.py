6c0216dbc8c2aaf3f6b748b8a8438be73b6453da
lukewalbeck <lukewalbeck@gmail.com>
2019-08-22 22:24:29 +0000

    more changes to db and settings

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%%-)3a9n(j7)bne55^d)fw6@r)hp9fxqcjj4wan+t5_@s864ha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lwalbeck.pythonanywhere.com']
 
